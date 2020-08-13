848a0f72c54fff09b2b8f058e228c8ca79c7336d
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-08-13 17:39:30 -0400

    added drum images

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ var numDrumBtn = document.querySelectorAll(".drum").length;
 for (var i = 0; i<numDrumBtn; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got clicked!!");
 
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
   });
 
 }
