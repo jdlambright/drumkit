b2da58ba9ebb2c34c4ba57c07a11165fb1c93b19
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-08-13 23:37:46 -0400

    update java

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ var numDrumBtn = document.querySelectorAll(".drum").length;
 for (var i = 0; i<numDrumBtn; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+      this.style.color("white");
 
-      var audio = new Audio("sounds/tom-1.mp3");
-      audio.play();
   });
 
 }
+
+//var audio = new Audio("sounds/tom-1.mp3");
+//audio.play();
